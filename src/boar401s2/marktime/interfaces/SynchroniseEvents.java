0ba427b862592b67e871f90c01e4851a20d2d348
boar401s2 <johnrobboard@gmail.com>
2013-11-29 16:02:35 +1000

    Removed duplicate Interface

--- src/boar401s2/marktime/interfaces/SynchroniseEvents.java
+++
@@ -1,9 +0,0 @@
-package boar401s2.marktime.interfaces;
-
-public interface SynchroniseEvents {
-
-	public void onStatusChange(String status);
-	public void onConnected();
-	public void onSquadFetched();
-	
-}
